9ebf26e55374206c00b3bedd020241528163ab4a
Gikai Andrews <gnaboys@gmail.com>
2024-02-17 00:01:27 -0500

    added montage for multimedia section and added carousel

--- bman-web/src/Multimedia/Multimedia.js
+++ bman-web/src/Multimedia/Multimedia.js
@@ -1,11 +1,17 @@
-import React from "react";
-import "./Multimedia.css";
+import React from 'react';
+import './Multimedia.css';
+import rlvid from '../videos/rl-montage.mp4';
 
 function Multimedia() {
   return (
     <div className="multimedia-section">
-      <div className="work">
+      <div className="editing-section">
         <h1>Editing</h1>
+        
+          <video autoPlay loop muted>
+            <source src={rlvid} type="video/mp4" className='video'></source>
+          </video>
+          {/* You can add more video elements here if needed */}
       </div>
     </div>
   );
